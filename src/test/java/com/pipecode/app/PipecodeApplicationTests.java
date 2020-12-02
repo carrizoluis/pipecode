67a30155d8ac6c68438e63ebb85d0c016e83837d
Luis Alberto Carrizo <carry_90@hotmail.com>
2020-12-02 19:30:29 -0300

    First commit

---
+++ src/test/java/com/pipecode/app/PipecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipecode.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
